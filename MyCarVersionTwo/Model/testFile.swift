3ea63a75f94969ddd02e94149940ac4e1f899b45
SalmaHKamal <salmahassan087@gmail.com>
2018-05-22 04:03:44 -0700

    add Model folder

---
+++ MyCarVersionTwo/Model/testFile.swift
@@ -0,0 +1,11 @@
+//
+//  testFile.swift
+//  MyCarVersionTwo
+//
+//  Created by Sayed Abdo on 5/22/18.
+//  Copyright © 2018 Sayed Abdo. All rights reserved.
+//
+
+import Foundation
+
+// this file is for testing only
